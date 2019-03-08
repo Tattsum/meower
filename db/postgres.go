a09467e69dbd0b2222592bfc012773d24f970916
Tattsum <kurohari35@gmail.com>
2019-03-08 17:14:24 +0900

    add a sql interface

---
+++ db/postgres.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+
+	_ "github.com/lib/pq"
+	"github.com/tattsum/meower/schema"
+)
+
+type PostgresRepository struct {
+	db *sql.DB
+}
+
+func NewPostgres(url string) (*PostgresRepository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	return &PostgresRepository{
+		db,
+	}, nil
+}
+
+func (r *PostgresRepository) Close() {
+	r.db.Close()
+}
+
+func (r *PostgresRepository) InsertMeow(ctx context.Context, meow schema.Meow) error {
+	_, err := r.db.Exec("INSERT INTO meows(id, body, create_at) VALUES($1, $2, $3)", meow.ID, meow.Body, meow.CreateAt)
+	return err
+}
+
+func (r *PostgresRepository) ListMeows(ctx context.Context, skip uint64, take uint64) ([]schema.Meow, error) {
+	rows, err := r.db.Query("SELECT * FROM meows ORDER id DESC OFFSET $1 LIMIT $2", skip, take)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Parse all rows into an array of Meows
+	meows := []schema.Meow{}
+	for rows.Next() {
+		meow := schema.Meow{}
+		if err = rows.Scan(&meow.ID, &meow.Body, &meow.CreatedAt); err == nil {
+			meows = append(meows, meow)
+		}
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return meows, nil
+}
