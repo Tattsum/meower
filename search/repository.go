0e9d7599c68b1a12b0294a21316328e742d36df1
Tattsum <kurohari35@gmail.com>
2019-03-12 19:53:48 +0900

    create search

---
+++ search/repository.go
@@ -0,0 +1,31 @@
+package search
+
+import (
+	"context"
+
+	"github.com/tattsum/meower/schema"
+)
+
+type Repository interface {
+	Close()
+	InsertMeow(ctx context.Context, meow schema.Meow) error
+	SearchMeows(ctx context.Context, query string, skip uint64, take uint64) ([]schema.Meow, error)
+}
+
+var impl Repository
+
+func SetRepository(repository Repository) {
+	impl = repository
+}
+
+func Close() {
+	impl.Close()
+}
+
+func InsertMeow(ctx context.Context, meow schema.Meow) error {
+	return impl.InsertMeow(ctx, meow)
+}
+
+func SearchMeows(ctx context.Context, query string, skip uint64, take uint64) ([]schema.Meow, error) {
+	return impl.SearchMeows(ctx, query, skip, take)
+}
