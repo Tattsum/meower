58f06ff2106150df340e89bf25f07da098006ffc
Tattsum <kurohari35@gmail.com>
2019-03-25 14:29:51 +0900

    create a main

---
+++ pusher-service/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/kelseyhightower/envconfig"
+	"github.com/tattsum/meower/event"
+	"github.com/tattsum/meower/retry"
+)
+
+type Config struct {
+	NatsAddress string `envconfig:"NATS_ADDRESS"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Connect to Nats
+	hub := newHub()
+	retry.ForeverSleep(2*time.Second, func(_ int) error {
+		es, err := event.NewNats(fmt.Sprintf("nats://%s", cfg.NatsAddress))
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		//Push message to clients
+		err = es.OnMeowCreated(func(m event.MeowCreatedMessage) {
+			log.Printf("Meow received: %v\n", m)
+			hub.broadcast(newMeowCreatedMessage(m.ID, m.Body, m.CreatedAt), nil)
+		})
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		event.SetEventStore(es)
+		return nil
+	})
+	defer event.Close()
+
+	// Run WebSocket server
+	go hub.run()
+	http.HandleFunc("/pusher", hub.handleWebSocket)
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
