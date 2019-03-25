6ef077b0ac8125216b2e275c28727c066a5c6041
Tattsum <kurohari35@gmail.com>
2019-03-25 16:18:11 +0900

    create a meow

---
+++ frontend/src/components/Meow.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <p class="card-text" v-html="body"></p>
+      <p class="card-text">
+        <small class="text-muted">
+          {{time}}
+        </small>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import timeago from 'timeago.js';
+export default {
+  props: ['meow'],
+  computed: {
+    body() {
+      return this.meow.body;
+    },
+    time() {
+      return timeago().format(Date.parse(this.meow.created_at));
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  margin-bottom: 1rem;
+}
+.card-body {
+  padding: 0.5rem;
+  p {
+    margin-bottom: 0;
+  }
+}
+</style>
